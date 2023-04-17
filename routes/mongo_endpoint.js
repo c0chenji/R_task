4e8c05461fd920675f12baf5befc3295f5bb69bb
Jarick <jarick@live.ca>
2023-04-17 01:02:59 -0400

    add the rest get methods and screen shots for mongodb compass and sample result

--- routes/mongo_endpoint.js
+++ routes/mongo_endpoint.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/myDatabase', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/takeHomeTask', { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log(error));
 
@@ -19,7 +19,18 @@ const dataSchema = new mongoose.Schema({
 });
 
 // Define data model
-const Data = mongoose.model('Data', dataSchema);
+const Data = mongoose.model('dataLog', dataSchema);
+
+// Helper function for time range query
+function applyTimeRange(startDate, endDate) {
+  let query = {};
+
+  if (startDate && endDate) {
+    query.originationTime = { $gte: startDate, $lte: endDate };
+  }
+
+  return query;
+}
 
 // Define route to get all data from MongoDB
 router.get('/', (req, res) => {
@@ -33,42 +44,63 @@ router.get('/phone/:phone', (req, res) => {
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
 
-  let query = { 'devices.phone': phone };
+  let query = { 
+    'devices.phone': phone,
+    ...applyTimeRange(startDate, endDate),
+  };
 
-  if (startDate && endDate) {
-    query.originationTime = { $gte: startDate, $lte: endDate };
-  }
+
   
   Data.find(query)
     .then((data) => res.json(data))
     .catch((error) => res.status(500).send(error.message));
 });
 
-// // Define route to get data by voicemail
-// router.get('/voicemail/:voicemail', (req, res) => {
-//   const voicemail = req.params.voicemail;
-//   const query = { 'devices.voicemail': voicemail, originationTime: { $gte: req.query.startDate, $lte: req.query.endDate } };
-//   Data.find(query)
-//     .then((data) => res.json(data))
-//     .catch((error) => res.status(500).send(error.message));
-// });
+router.get('/voicemail/:voicemail', (req, res) => {
+  const voicemail = req.params.voicemail;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
 
-// // Define route to get data by user id
-// router.get('/userId/:userId', (req, res) => {
-//   const userId = req.params.userId;
-//   const query = { userId: userId, originationTime: { $gte: req.query.startDate, $lte: req.query.endDate } };
-//   Data.find(query)
-//     .then((data) => res.json(data))
-//     .catch((error) => res.status(500).send(error.message));
-// });
+  let query = { 
+    'devices.voicemail': voicemail,
+    ...applyTimeRange(startDate, endDate)
+  };
 
-// Define route to get data by cluster
-router.get('/cluster/:clusterId', (req, res) => {
-  const clusterId = req.params.clusterId;
-  const query = { clusterId: clusterId, originationTime: { $gte: req.query.startDate, $lte: req.query.endDate } };
+  
   Data.find(query)
     .then((data) => res.json(data))
     .catch((error) => res.status(500).send(error.message));
 });
+
+router.get('/clusterId/:clusterId', (req, res) => {
+  const clusterId = req.params.clusterId;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  const query = {
+    clusterId: clusterId,
+    ...applyTimeRange(startDate, endDate)
+  };
+
+  Data.find(query)
+    .then((data) => res.json(data))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+router.get('/userId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  const query = {
+    userId: userId,
+    ...applyTimeRange(startDate, endDate)
+  };
+
+  Data.find(query)
+    .then((data) => res.json(data))
+    .catch((error) => res.status(500).send(error.message));
+});
+
 
 module.exports = router;
--- routes/read_json.js
+++ routes/read_json.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const fs = require('fs');
 
 
-// Read json from route
+// Read json sample json file and push it to url
 router.get('/', (req, res) => {
-  readJsonFile('sample_data/sample.json')
+  read_json_file('sample_data/sample.json')
     .then((jsonData) => res.json(jsonData))
     .catch((error) => res.status(500).send(error.message));
 });
 
-function readJsonFile(filename) {
+function read_json_file(filename) {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
       if (error) {
