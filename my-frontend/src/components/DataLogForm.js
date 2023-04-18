5fcf7cc3924cfa735cc417469ae8959a95fcdf66
Jarick <jarick@live.ca>
2023-04-17 22:53:57 -0400

    Updated DataLogForm

--- my-frontend/src/components/DataLogForm.js
+++ my-frontend/src/components/DataLogForm.js
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Box, CircularProgress } from '@material-ui/core';
 
-function DataLogForm() {
+const DataLogForm = ({ onSubmit, isLoading }) => {
+  const [phone, setPhone] = useState('');
+  const [voicemail, setVoicemail] = useState('');
+  const [userId, setUserId] = useState('');
+  const [clusterId, setClusterId] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-  const [parameter, setParameter] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Implement submit functionality
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ phone, voicemail, userId, clusterId, startDate, endDate });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField label="Start Date" value={startDate} onChange={(event) => setStartDate(event.target.value)} />
-      <TextField label="End Date" value={endDate} onChange={(event) => setEndDate(event.target.value)} />
-      <TextField label="Parameter" value={parameter} onChange={(event) => setParameter(event.target.value)} />
-      <Button type="submit" variant="contained" color="primary">Submit</Button>
-    </form>
+    <Box>
+      <form onSubmit={handleSubmit}>
+        <TextField label="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <TextField label="Voicemail" value={voicemail} onChange={(e) => setVoicemail(e.target.value)} />
+        <TextField label="User ID" value={userId} onChange={(e) => setUserId(e.target.value)} />
+        <TextField label="Cluster ID" value={clusterId} onChange={(e) => setClusterId(e.target.value)} />
+        <TextField type="date" label="Start Date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+        <TextField type="date" label="End Date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+        <Button variant="contained" color="primary" type="submit" disabled={isLoading}>
+          {isLoading ? <CircularProgress size={24} /> : 'Search'}
+        </Button>
+      </form>
+    </Box>
   );
-}
+};
 
 export default DataLogForm;
